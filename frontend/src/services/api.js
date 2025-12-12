e1ab2fb81e3e5aa22e05742b3c9fd03cac74c8e7
PriyanshuGeTRekT <priyanshu.py98@gmail.com>
2025-12-12 19:32:46 +0530

    Add API service using Axios

---
+++ frontend/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:5000/api',
+});
+
+// Simple API wrapper
+export const getUsers = () => api.get('/users');
+export const getUser = (id) => api.get(`/users/${id}`);
+export const createUser = (data) => api.post('/users', data);
+export const updateUser = (id, data) => api.put(`/users/${id}`, data);
+export const deleteUser = (id) => api.delete(`/users/${id}`);
+export const getAnalytics = () => api.get('/analytics/regions');
+
+export default api;
